3413e92ead0da98b563f865072c20c17ef4d2ef9
Luiz Costa <phelip@thenets.org>
2017-11-10 12:46:39 -0200

    scheming front-end files

--- ckanext/dadosgovbrschema/plugin.py
+++ ckanext/dadosgovbrschema/plugin.py
@@ -1,6 +1,8 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+# Custom helper
+from ckanext.dadosgovbrschema import helpers
 
 class DadosgovbrschemaPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -11,3 +13,21 @@ class DadosgovbrschemaPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'dadosgovbrschema')
+
+    
+    # Registro dos helpers
+    # =======================================================
+    def get_helpers(self):
+        '''Register all functions
+
+        '''
+        
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {
+            # Scheming
+            'dadosgovbr_get_schema_name': helpers.scheming.get_schema_name,
+            'dadosgovbr_get_schema_title': helpers.scheming.get_schema_title
+        }
+        
